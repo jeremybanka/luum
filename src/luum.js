c7dda98c1b82f6a17938ac74cfa620ab5a8f1355
Jeremy Banka <jeremybanka@gmail.com>
2020-10-25 17:25:55 -0700

    add more exported functions

--- src/luum.js
+++ src/luum.js
@@ -1,6 +1,15 @@
 import hexToSpec from './import/hexToSpec'
+import specToHex from './export/specToHex'
+import specToHexFixLimit from './export/specToHexFixLimit'
+import gradientsToHexArrays from './export/gradientsToHexArrays' // eslint-disable-line
+import getStandout from './getStandout.js'
+import getOffset from './getOffset.js'
 
 export {
   hexToSpec,
+  getStandout,
+  getOffset,
+  specToHex,
+  specToHexFixLimit,
+  gradientsToHexArrays,
 }
-
