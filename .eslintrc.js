5858b87e60c64c49293a6e7a73c65d5e6d7ca364
Jeremy Banka <hello@jeremybanka.com>
2021-10-11 00:44:13 -0700

    🔧 add better rules for quotes and restricted-syntax

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,117 +1,143 @@
 module.exports = {
-  root: true,
-  extends: [
-    'airbnb',
-  ],
-  parser: 'babel-eslint',
+  extends: [`airbnb`],
+  plugins: [`jest`],
+  parser: `babel-eslint`,
   parserOptions: {
     ecmaVersion: 12,
-    ecmaFeatures: {
-      jsx: true,
-    },
-    sourceType: 'module',
   },
   env: {
-    'es6': true,
-    'worker': true,
-    'browser': true,
-    'jest': true,
+    browser: true,
+    es2021: true,
+    jest: true,
   },
-  plugins: [],
-  "settings": {
+  settings: {
     "import/resolver": {
-      "node": {
-        "extensions": [".js"],
+      node: {
+        extensions: [`.js`],
       },
     },
   },
   rules: {
-    'no-case-declarations': 'off',
-    'no-fallthrough': 'off',
-    'arrow-parens': ['error', 'as-needed'],
-    'comma-dangle': ['error', {
-      'arrays': 'always-multiline',
-      'objects': 'always-multiline',
-      'imports': 'always-multiline',
-      'exports': 'always-multiline',
-      'functions': 'only-multiline',
-    }],
-    'consistent-return': 'off',
-    'curly': ['error', 'multi-line', 'consistent'],
-    'function-paren-newline': 'off',
-    'implicit-arrow-linebreak': 'off',
-    'jsx-quotes': 'off',
-    'keyword-spacing': ['error', {
-      'overrides': {
-        'if': { 'after': false },
-        'while': { 'after': false },
-        'for': { 'after': false },
-        'switch': { 'after': false },
-        'catch': { 'after': false },
-      },
-    }],
-    'max-classes-per-file': 'off',
-    'max-len': ['error', {
-      'code': 115,
-      'ignoreRegExpLiterals': true,
-    }],
-    'no-confusing-arrow': 'off',
-    'no-console': 'off',
-    'no-continue': 'off',
-    'no-else-return': ['error', {
-      'allowElseIf': true,
-    }],
-    'no-mixed-operators': 'off',
-    'no-multi-spaces': ['error', {
-      'ignoreEOLComments': true,
-      'exceptions': {
-        'Property': true,
-        'VariableDeclarator': true,
-        'ImportDeclaration': true,
-      },
-    }],
-    'no-nested-ternary': 'off',
-    'no-param-reassign': 'off',
-    'no-plusplus': 'off',
-    'no-return-assign': 'off',
-    'no-shadow': 'off',
-    'no-undef-init': 'off',
-    'no-underscore-dangle': 'off',
-    'no-unused-expressions': ['error', {
-      'allowShortCircuit': true,
-      'allowTernary': true,
-    }],
-    'no-use-before-define': 'off',
-    'nonblock-statement-body-position': 'off',
-    'object-curly-newline': ['error', {
-      'consistent': true,
-    }],
-    'operator-linebreak': 'off',
-    'prefer-destructuring': ["error",
+    "arrow-parens": [`error`, `as-needed`],
+    "comma-dangle": [
+      `error`,
       {
-        'VariableDeclarator': {
-          'array': false,
-          'object': true,
-        },
-        'AssignmentExpression': {
-          'array': false,
-          'object': false,
-        },
-      },
-      {
-        'enforceForRenamedProperties': false,
+        arrays: `always-multiline`,
+        objects: `always-multiline`,
+        imports: `always-multiline`,
+        exports: `always-multiline`,
+        functions: `only-multiline`,
       },
     ],
-    'prefer-object-spread': 'off',
-    'quotes': 'off',
-    'quote-props': ['error', 'as-needed', { 'unnecessary': false }],
-    'semi': ['error', 'never'],
-    'semi-style': ['error', 'first'],
-
-    // Begin import rules
-    'import/no-extraneous-dependencies': 'off',
-    'import/no-named-as-default': 'off',
-    'import/prefer-default-export': 'off',
-    'import/extensions': ['error', 'never'],
+    "consistent-return": `off`,
+    "curly": [`error`, `multi-line`, `consistent`],
+    "function-paren-newline": `off`,
+    "implicit-arrow-linebreak": `off`,
+    "import/extensions": [
+      `error`,
+      `ignorePackages`,
+      {
+        js: `never`,
+        jsx: `never`,
+        ts: `never`,
+        tsx: `never`,
+      },
+    ],
+    "import/prefer-default-export": `off`,
+    "jsx-quotes": [`error`, `prefer-double`],
+    "keyword-spacing": [
+      `error`,
+      {
+        overrides: {
+          if: { after: true },
+          while: { after: true },
+          for: { after: true },
+          switch: { after: true },
+          catch: { after: true },
+        },
+      },
+    ],
+    "max-classes-per-file": `off`,
+    "max-len": [
+      `error`,
+      {
+        code: 81,
+        ignoreRegExpLiterals: true,
+      },
+    ],
+    "no-confusing-arrow": `off`,
+    "no-console": `off`,
+    "no-continue": `off`,
+    "no-else-return": [
+      `error`,
+      {
+        allowElseIf: true,
+      },
+    ],
+    "no-mixed-operators": `off`,
+    "no-multi-spaces": [
+      `error`,
+      {
+        ignoreEOLComments: true,
+        exceptions: {
+          Property: true,
+          VariableDeclarator: true,
+          ImportDeclaration: true,
+        },
+      },
+    ],
+    "no-nested-ternary": `off`,
+    "no-param-reassign": `off`,
+    "no-plusplus": `off`,
+    "no-restricted-syntax": [
+      `error`,
+      `ForInStatement`,
+      `LabeledStatement`,
+      `WithStatement`,
+    ],
+    "no-return-assign": `off`,
+    "no-shadow": `off`,
+    "no-undef-init": `off`,
+    "no-underscore-dangle": `off`,
+    "no-unused-expressions": [
+      `error`,
+      {
+        allowShortCircuit: true,
+        allowTernary: true,
+      },
+    ],
+    "no-use-before-define": `off`,
+    "nonblock-statement-body-position": `off`,
+    "object-curly-newline": [
+      `error`,
+      {
+        consistent: true,
+      },
+    ],
+    "operator-linebreak": [
+      `error`,
+      `before`,
+      { overrides: { "?": `before`, ":": `before` } },
+    ],
+    "prefer-destructuring": [
+      `error`,
+      {
+        VariableDeclarator: {
+          array: false,
+          object: true,
+        },
+        AssignmentExpression: {
+          array: false,
+          object: false,
+        },
+      },
+      {
+        enforceForRenamedProperties: false,
+      },
+    ],
+    "quotes": [`error`, `backtick`],
+    "quote-props": [`error`, `consistent`, { unnecessary: false }],
+    "semi": [`error`, `never`],
+    "semi-style": [`error`, `first`],
   },
 }
